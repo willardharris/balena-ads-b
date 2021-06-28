490a8bbd64851e0b1f35872ef61dbfff14c037c8
Ketil Moland Olsen <ketil@Ketils-MacBook-Pro.local>
2021-06-28 11:50:25 +0200

    Worked around Radarbox keyserver issue (Fixed #31).

--- radarbox/radarbox_installer.sh
+++ radarbox/radarbox_installer.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Import our key to apt-key
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 1D043681
+# Workaround to mitigate Radarbox keyserver issue 1/3 (commented out)
+# apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 1D043681
 
 # Move old source
 /bin/rm -f /etc/apt/sources.list.d/rb24.list
@@ -16,10 +17,14 @@ echo System Architecture: $arch
 # If host architecture is i386 or amd64, install armhf-version of RadarBox and run it throug software emulation.
 if [ "$arch" = "i386" ] || [ "$arch" = "amd64" ]; then 
 	dpkg --add-architecture armhf
-		apt update && apt install -y --no-install-recommends \
+# Workaround to mitigate Radarbox keyserver issue 2/3
+#		apt update  && apt install -y --no-install-recommends \
+		apt update --allow-unauthenticated --allow-insecure-repositories && apt install -y --allow-unauthenticated --no-install-recommends \
 	    rbfeeder:armhf qemu-user qemu-user-static binfmt-support libc6-armhf-cross
 else 
-	apt update && apt install -y --no-install-recommends \
+# Workaround to mitigate Radarbox keyserver issue 3/3
+#   apt update && apt install -y --no-install-recommends \
+	apt update --allow-unauthenticated --allow-insecure-repositories && apt install -y --allow-unauthenticated --no-install-recommends \
 	    rbfeeder
 fi
 
