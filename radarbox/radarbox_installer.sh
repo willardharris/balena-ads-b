077ba206215b17af9fa7c0377c5cfa8ed9caba47
Ketil Moland Olsen <ketil@Wonderbook-2016.home>
2021-11-06 17:36:33 +0100

    Created RadarBox arm64 workaround to get latest version.

--- radarbox/radarbox_installer.sh
+++ radarbox/radarbox_installer.sh
@@ -13,11 +13,16 @@ echo 'deb https://apt.rb24.com/ buster main' > /etc/apt/sources.list.d/rb24.list
 arch="$(dpkg --print-architecture)"
 echo System Architecture: $arch
 
-# If host architecture is i386 or amd64, install armhf-version of RadarBox and run it throug software emulation.
+# If host architecture is i386, amd64, or arm6 install armhf-version of RadarBox and run it throug software emulation.
 if [ "$arch" = "i386" ] || [ "$arch" = "amd64" ]; then 
 	dpkg --add-architecture armhf
 		apt update  && apt install -y --no-install-recommends \
 	    rbfeeder:armhf qemu-user qemu-user-static binfmt-support libc6-armhf-cross
+# This elif is hopefully a temporary workaround, as the newest RadarBox library is not currently available for arm64.	    
+elif [ "$arch" = "arm64" ]; then 
+	dpkg --add-architecture armhf
+		apt update  && apt install -y --no-install-recommends \
+	    rbfeeder:armhf
 else 
     apt update && apt install -y --no-install-recommends \
 	    rbfeeder
