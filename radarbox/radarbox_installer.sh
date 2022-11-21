92e8eee60c333c4c6b79b62045ddd783b914f8b1
Schuby <schubydoo@users.noreply.github.com>
2022-11-21 11:44:27 -0800

    App Updates and Balena Blocks Changes (#68)
    * Update FR24Feed Version (#3)
    
    From 1.0.29-10 to 1.0.30-3 (https://repo-feed.flightradar24.com/CHANGELOG.md)
    
    * Update MLAT Tag to latest (#4)
    
    From 0.3.9 to 0.4.2 (https://github.com/adsbxchange/mlat-client/tags).  This will also match the mlat container.
    
    * Update READSB Version
    
    Newest commit: a25f8ea9c8427ad1bea82f244b7967ac8cd3153f
    
    * Update repo source file (and small typo fix)
    
    Repo source from buster to bullseye, and a small typo fix in the commented code.
    
    * Update traefik to v2.9.5
    
    Bumped traefik to v2.9.5 (https://github.com/traefik/traefik/releases/tag/v2.9.5)
    
    * Update traefik github URL
    
    Updated the github URL for traefik
    
    * Update minimum binary used (armv6)
    
    It looks like Traefik isn't making armv5 binaries anymore.  I'm not sure if this will break certain devices.  Updating to armv6
    
    * Updating URL (balenalabs)
    
    It seems that 'balenablocks' no longer works, and should now be 'balenalabs'.  (see https://github.com/balena-labs-projects/browser)
    
    * Updating URL (balenalabs)
    
    It seems that 'balenablocks' no longer works, and should now be 'balenalabs'.  (see https://github.com/balena-labs-projects/fbcp)
    
    * Updating URL (balenalabs)
    
    It seems that 'balenablocks' no longer works, and should now be 'balenalabs'.  (see https://github.com/balena-labs-projects/wifi-connect)

--- radarbox/radarbox_installer.sh
+++ radarbox/radarbox_installer.sh
@@ -8,12 +8,12 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 1D043681
 /bin/rm -f /etc/apt/sources.list.d/rb24.list
 
 # Create a new debian repository source file
-echo 'deb https://apt.rb24.com/ buster main' > /etc/apt/sources.list.d/rb24.list
+echo 'deb https://apt.rb24.com/ bullseye main' > /etc/apt/sources.list.d/rb24.list
 
 arch="$(dpkg --print-architecture)"
 echo System Architecture: $arch
 
-# If host architecture is i386, amd64, or arm6 install armhf-version of RadarBox and run it throug software emulation.
+# If host architecture is i386, amd64, or arm6 install armhf-version of RadarBox and run it through software emulation.
 if [ "$arch" = "i386" ] || [ "$arch" = "amd64" ]; then 
 	dpkg --add-architecture armhf
 		apt update  && apt install -y --no-install-recommends \
--- traefik/traefik_installer.sh
+++ traefik/traefik_installer.sh
@@ -9,9 +9,9 @@ if [ "$arch" = "arm64" ]; then
 elif [ "$arch" = "amd64" ]; then 
 	traefik_arch="amd64"
 else 
-	traefik_arch="armv5" 
+	traefik_arch="armv6" 
 fi
 
 traefik_packet="traefik_v${TRAEFIK_VERSION}_linux_$traefik_arch.tar.gz"
 
-cd /tmp/ && wget --quiet -O traefik.tar.gz "https://github.com/containous/traefik/releases/download/v${TRAEFIK_VERSION}/$traefik_packet"; 
+cd /tmp/ && wget --quiet -O traefik.tar.gz "https://github.com/traefik/traefik/releases/download/v${TRAEFIK_VERSION}/$traefik_packet"; 
