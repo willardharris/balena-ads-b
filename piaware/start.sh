a7f834b870cbafa99d12475844c44fcccdd462e4
Ketil Moland Olsen <ketil@Wonderbook38989.home>
2024-04-28 15:46:50 +0200

    Small change to UAT config.

--- piaware/start.sh
+++ piaware/start.sh
@@ -56,8 +56,8 @@ echo " "
 # If dump978-fa is disabled through config, disable it in piaware.
 if [[ "$UAT_ENABLED" = "true" ]]; then
         /usr/bin/piaware-config uat-receiver-type sdr
-        /usr/bin/piaware-config uat-receiver-host dump978-fa
-        /usr/bin/piaware-config uat-receiver-port 30978
+        /usr/bin/piaware-config uat-receiver-host "${UAT_RECEIVER_HOST}"
+        /usr/bin/piaware-config uat-receiver-port "${UAT_RECEIVER_PORT}"
 else
         /usr/bin/piaware-config uat-receiver-type none
 
